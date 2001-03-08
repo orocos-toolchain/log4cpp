6bfe07ebabfbd3ff60b3670fe8239c806c073945
Cedric Le Goater <cedric@legoater.com>
2001-03-08 08:23:43 +0000

    Added CPU clock support with CLOCK_USE_CPU environment variable.

--- tests/Clock.cpp
+++ tests/Clock.cpp
@@ -4,27 +4,60 @@ static const char rcsid[] = "$Id$";
  * See the COPYING file for the terms of usage and distribution.
  */
 
-#include <sys/time.h>	// struct timeval
+#include <sys/time.h>			// for struct timeval
+#ifdef __osf__
+#    include <machine/builtins.h>       // for __RPCC()
+#elif __linux__
+#    include <asm/msr.h>		// for rdtscl()
+#endif
+#include <cstdlib>
+#include <iostream>
 
 #include "Clock.hh"
 
-const usec_t UsecPerSec = INT64_CONSTANT(1000000);
+namespace 
+{
+    const usec_t UsecPerSec = INT64_CONSTANT(1000000);
+}
+
+bool Clock::UsingCPU  = ::getenv("CLOCK_USE_CPU") ? true : false;
 
 // -----------------------------------------------------------------------------
 usec_t Clock::time(void)
 {
-    struct timeval tv;
-    
-    gettimeofday(&tv, NULL);
-    
-    return (usec_t) (tv.tv_sec * UsecPerSec + tv.tv_usec);
+    if (UsingCPU) {
+	static bool warn = true;
+		
+	if (warn) {
+	    std::cout << "Using CPU clock." << std::endl;
+	    warn = false;
+	}
+
+#ifdef __osf__
+	return (usec_t) __RPCC();
+#elif __linux__
+	{
+	    unsigned long tsc;
+			
+	    rdtscl(tsc);
+	    return (usec_t) tsc;
+	}
+#else
+#    error CPU clock not implemented for this architecture
+#endif	
+    } else {
+	struct timeval tv;
+	
+	gettimeofday(&tv, NULL);	
+	return (usec_t) (tv.tv_sec * UsecPerSec + tv.tv_usec);
+    }
 }
 
 // -----------------------------------------------------------------------------
 Clock::Clock(void)
     : _start(0),
       _elapsed(0),
-      _active(true) 
+      _active(false) 
 {
     start();
 }
@@ -36,28 +69,29 @@ Clock::~Clock(void)
 }
 
 // -----------------------------------------------------------------------------
-usec_t Clock::start(void)
-{
-    _active = true;
-
-    return (_start = time());
-}
-
-// -----------------------------------------------------------------------------
-usec_t Clock::elapsed(void)
+usec_t Clock::elapsed(void) const
 {
     if (!active())
 	return _elapsed;
 
-    return (_elapsed = time() - _start);
+    return time() - _start;
+}
+
+
+// -----------------------------------------------------------------------------
+usec_t Clock::start(void)
+{
+    _active = true;
+
+    return _start = time();
 }
 
 // -----------------------------------------------------------------------------
 usec_t Clock::stop(void)
 {
-    elapsed();
-
-    _active = false;
+    _elapsed = elapsed();
+    _active  = false;
 
     return _elapsed;
 }
+
--- tests/Clock.hh
+++ tests/Clock.hh
@@ -6,8 +6,6 @@
 #ifndef __CLOCK_H
 #define __CLOCK_H
 
-#include <iostream>
-
 #ifdef __osf__
 typedef long usec_t;    /* number of microseconds since 1970/01/01 */
 #define INT64_CONSTANT(val)  (val##L)
@@ -20,19 +18,20 @@ typedef long long int usec_t;
 
 class Clock
 {
- public:
+public:
+    static bool		UsingCPU;
     static usec_t 	time(void);
 
-    explicit		Clock(void);
-    virtual		~Clock(void);
+    			Clock(void);
+    			~Clock(void);
 
     bool		active(void) const { return _active; }
+    usec_t		elapsed(void) const;
     usec_t		start(void);
     usec_t		reset(void) { return start(); }
-    usec_t		elapsed(void);
     usec_t		stop(void);
 
- private:
+private:
     usec_t		_start;    
     usec_t		_elapsed;
     bool		_active;
