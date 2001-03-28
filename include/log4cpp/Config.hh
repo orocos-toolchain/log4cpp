429b84074f8e50ff8486ee29d4c972e370bebd82
Cedric Le Goater <cedric@legoater.com>
2001-03-28 06:58:23 +0000

    Added Config.hh for MSVC++.

---
+++ include/log4cpp/Config.hh
@@ -0,0 +1,16 @@
+//
+// Config.hh
+//
+// This is a generated file.  Please modify `configure.in'
+
+#ifndef _LOG4CPP_CONFIG_HH
+#define _LOG4CPP_CONFIG_HH
+
+#if defined(_MSC_VER)
+#    pragma warning( disable : 4786 )
+     using namespace std;
+#endif
+
+/* define if you have new iostreams */
+#define LOG4CPP_HAVE_STDIOSTREAM 1
+#endif
