98bc04e702be1cebe704eaa1ead70f225f967022
Uwe J.ger <jaeger@varial.de>
2001-06-06 12:35:19 +0000

    added defines

--- include/log4cpp/config-win32.h
+++ include/log4cpp/config-win32.h
@@ -12,33 +12,45 @@
 #endif
 
 /* Define if you have the <unistd.h> header file.  */
-/* #undef LOG4CPP_HAVE_UNISTD_H */ 
+/* #undef LOG4CPP_HAVE_UNISTD_H */
 
 /* Define if you have the idsa library (-lidsa).  */
 /* #undef LOG4CPP_HAVE_LIBIDSA */
 
 /* Name of package */
-#ifndef LOG4CPP_PACKAGE 
-#define LOG4CPP_PACKAGE  "log4cpp" 
+#ifndef LOG4CPP_PACKAGE
+#define LOG4CPP_PACKAGE  "log4cpp"
 #endif
 
 /* Version number of package */
-#ifndef LOG4CPP_VERSION 
-#define LOG4CPP_VERSION  "0.2.4" 
+#ifndef LOG4CPP_VERSION
+#define LOG4CPP_VERSION  "0.2.4"
 #endif
 
 /* define if the compiler implements namespaces */
-#ifndef LOG4CPP_HAVE_NAMESPACES 
-#define LOG4CPP_HAVE_NAMESPACES   
+#ifndef LOG4CPP_HAVE_NAMESPACES
+#define LOG4CPP_HAVE_NAMESPACES 1
 #endif
 
 /* define if the compiler has stringstream */
-#ifndef LOG4CPP_HAVE_SSTREAM 
-#define LOG4CPP_HAVE_SSTREAM   
+#ifndef LOG4CPP_HAVE_SSTREAM
+#define LOG4CPP_HAVE_SSTREAM 1
 #endif
 
 /* define if the C library has snprintf */
-/* #define LOG4CPP_HAVE_SNPRINTF */   
+#ifndef LOG4CPP_HAVE_SNPRINTF
+#define LOG4CPP_HAVE_SNPRINTF 1
+#endif
+
+/* define to get around problems with ERROR in windows.h */
+#ifndef LOG4CPP_FIX_ERROR_COLLISION
+#define LOG4CPP_FIX_ERROR_COLLISION 1
+#endif
+
+/* define WIN32 for Borland */
+#ifndef WIN32
+#define WIN32
+#endif
 
 /* _INCLUDE_LOG4CPP_CONFIG_WIN32_H */
 #endif
