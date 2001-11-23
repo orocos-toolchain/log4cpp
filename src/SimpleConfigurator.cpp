eeebe6f22738dd1964a535a2a4f3457268937992
Bastiaan Bakker <bastiaan.bakker@lifeline.nl>
2001-11-23 18:03:58 +0000

    added support for comments in config file (starting with a '#'). added support for SyslogAppender. use Priority::getPriorityValue() to convert priorities.

--- src/SimpleConfigurator.cpp
+++ src/SimpleConfigurator.cpp
@@ -1,5 +1,5 @@
 /*
- * Log4cppConfigurator.cpp
+ * SimpleConfigurator.cpp
  *
  * Copyright 2001, Glen Scott. All rights reserved.
  *
@@ -11,12 +11,6 @@
 #include <unistd.h>
 #endif
 #include <iostream>
-
-#ifdef LOG4CPP_HAVE_SSTREAM
-#include <sstream>
-#else
-#error not backported yet
-#endif
 #include <string>
 #include <fstream>
 
@@ -29,38 +23,48 @@
 #include "log4cpp/SimpleLayout.hh"
 #include "log4cpp/Priority.hh"
 #include "log4cpp/NDC.hh"
-#ifdef HAVE_PATTERN_LAYOUT
 #include "log4cpp/PatternLayout.hh"
-#endif
-
 #include "log4cpp/SimpleConfigurator.hh"
+#if LOG4CPP_HAVE_SYSLOG
+#include "log4cpp/SyslogAppender.hh"
+#endif
 
 namespace log4cpp {
 
     ConfigureFailure::ConfigureFailure(const std::string& reason) :
         std::runtime_error(reason) {
     }
-
+    
     void SimpleConfigurator::configure(const std::string& initFileName) throw (ConfigureFailure) {
         std::ifstream initFile(initFileName.c_str());
-
+        
         if (!initFile) {
-            throw ConfigureFailure(std::string("File ") + initFileName + " does not exist");
+            throw ConfigureFailure(std::string("Config File ") + initFileName + " does not exist or is unreadable");
         }
-
+        
         std::string nextCommand;
         std::string categoryName;
+        
+        while (initFile >> nextCommand) {
+            /* skip comment lines */
+            if (nextCommand[0] == '#') {
+                std::string dummy;
+                std::getline(initFile, dummy);
+                continue;
+            }
+            /* stop on missing categoryName */
+            if (!(initFile >> categoryName))
+                break;
 
-        while (initFile >> nextCommand >> categoryName) {
             log4cpp::Category& category =
                 (categoryName.compare("root") == 0) ?
                 log4cpp::Category::getRoot() :
                 log4cpp::Category::getInstance(categoryName);
-
+            
             if (nextCommand.compare("appender") == 0) {
                 std::string layout;
                 std::string appenderName;
-
+                
                 if (initFile >> layout >> appenderName) {
                     log4cpp::Appender* appender;
                     if (appenderName.compare("file") == 0) {
@@ -68,73 +72,76 @@ namespace log4cpp {
                         if (!(initFile >> logFileName)) {
                             throw ConfigureFailure("Missing filename for log file logging configuration file for category: " + categoryName);
                         }
-                        appender = new log4cpp::FileAppender(std::string("default"), logFileName);
+                        appender = new log4cpp::FileAppender(categoryName, logFileName);
                     }
                     else if (appenderName.compare("console") == 0) {
                         appender =
-                            new log4cpp::OstreamAppender("default", &std::cout);
-                } else {
-                    throw ConfigureFailure("Invalid appender name (" +
-                                           appenderName +
-                                           ") in logging configuration file for category: " +
-                                           categoryName);
-                }
-                if (layout.compare("basic") == 0)
-                    appender->setLayout(new log4cpp::BasicLayout());
-                else if (layout.compare("simple") == 0)
-                    appender->setLayout(new log4cpp::SimpleLayout());
-#ifdef HAVE_PATTERN_LAYOUT
-                else if (layout.compare("pattern") == 0) {
-                    log4cpp::PatternLayout *layout =
-                        new log4cpp::PatternLayout();
-                    char pattern[1000];
-                    initFile.getline(pattern, 1000);
-                    layout->setConversionPattern(std::string(pattern));
-                    appender->setLayout(layout);
-                }
+                            new log4cpp::OstreamAppender(categoryName, &std::cout);
+                    }
+#if LOG4CPP_HAVE_SYSLOG
+                    else if (appenderName.compare("syslog") == 0) {
+                        std::string syslogName;
+                        int facility;
+                        if (!(initFile >> syslogName)) {
+                            throw ConfigureFailure("Missing syslogname for SysLogAppender for category: " + categoryName);
+                        }
+                        if (!(initFile >> facility)) {
+                            facility = LOG_USER;
+                        }
+                        appender =
+                            new log4cpp::SyslogAppender(categoryName, syslogName, facility);
+                    } 
 #endif
-                else {
-                    throw ConfigureFailure("Invalid layout (" + layout +
-                        ") in logging configuration file for category: " +
-                        categoryName);
+                    else {
+                        throw ConfigureFailure("Invalid appender name (" +
+                                               appenderName +
+                                               ") in logging configuration file for category: " +
+                                               categoryName);
+                    }
+                    if (layout.compare("basic") == 0)
+                        appender->setLayout(new log4cpp::BasicLayout());
+                    else if (layout.compare("simple") == 0)
+                        appender->setLayout(new log4cpp::SimpleLayout());
+                    else if (layout.compare("pattern") == 0) {
+                        log4cpp::PatternLayout *layout =
+                            new log4cpp::PatternLayout();
+                        char pattern[1000];
+                        initFile.getline(pattern, 1000);
+                        layout->setConversionPattern(std::string(pattern));
+                        appender->setLayout(layout);
+                    }
+                    else {
+                        throw ConfigureFailure("Invalid layout (" + layout +
+                                               ") in logging configuration file for category: " +
+                                               categoryName);
+                    }
+                    category.setAppender(appender);
                 }
-                category.setAppender(appender);
             }
-        }
-        else if (nextCommand.compare("priority") == 0) {
-            std::string priority;
-            if (!(initFile >> priority)) {
-                throw ConfigureFailure("Missing priority in logging configuration file for category: " + categoryName);
+            else if (nextCommand.compare("priority") == 0) {
+                std::string priority;
+                if (!(initFile >> priority)) {
+                    throw ConfigureFailure("Missing priority in logging configuration file for category: " + categoryName);
+                }
+                
+                try {
+                    category.setPriority(log4cpp::Priority::getPriorityValue(priority));
+                } catch(std::invalid_argument) {
+                    throw ConfigureFailure("Invalid priority ("+priority+") in logging configuration file for category: "+categoryName);
+                }
             }
-            if (priority.compare("info") == 0) {
-                category.setPriority(log4cpp::Priority::INFO);
-            }
-            else if (priority.compare("debug") == 0) {
-                category.setPriority(log4cpp::Priority::DEBUG);
-            }
-            else if (priority.compare("warn") == 0) {
-                category.setPriority(log4cpp::Priority::WARN);
-            }
-            else if (priority.compare("error") == 0) {
-                category.setPriority(log4cpp::Priority::ERROR);
-            }
-	        else if (priority.compare("fatal") == 0) {
-                category.setPriority(log4cpp::Priority::FATAL);
+            else if (nextCommand.compare("category") == 0) {
+                /*
+                  This command means we should "refer" to the category
+                  (in order to have it created). We've already done this
+                  in common setup code for all commands.
+                */
             }
             else {
-                throw ConfigureFailure("Invalid priority ("+priority+") in logging configuration file for category: "+categoryName);
+                throw ConfigureFailure("Invalid format in logging configuration file. Command: " + nextCommand);
             }
         }
-        else if (nextCommand.compare("category") == 0) {
-            // This command means we should "refer" to the category (in order to have it
-            // created).
-            // We've already done this in common setup code for all commands.
-        }
-        else {
-            throw ConfigureFailure("Invalid format in logging configuration file. Command: " + nextCommand);
-        }
     }
-}
 }
 
 
