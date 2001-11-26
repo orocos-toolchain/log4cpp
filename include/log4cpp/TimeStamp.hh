2e7960e3b72d725e7163063a3c4b7b638e059695
Bastiaan Bakker <bastiaan.bakker@lifeline.nl>
2001-11-26 01:14:07 +0000

    added micro second precise time stamp.

---
+++ include/log4cpp/TimeStamp.hh
@@ -0,0 +1,59 @@
+/*
+ * TimeStamp.hh
+ *
+ * Copyright 2001, LifeLine Networks BV (www.lifeline.nl). All rights reserved.
+ * Copyright 2001, Bastiaan Bakker. All rights reserved.
+ *
+ * See the COPYING file for the terms of usage and distribution.
+ */
+
+#ifndef _LOG4CPP_TIMESTAMP_HH
+#define _LOG4CPP_TIMESTAMP_HH
+
+#include "log4cpp/Portability.hh"
+#include "log4cpp/Export.hh"
+
+/**
+ * A simple TimeStamp abstraction
+ **/
+namespace log4cpp {
+
+    class LOG4CPP_EXPORT TimeStamp {
+        public:
+        /**
+           Constructs a TimeStamp representing 'now'.
+        **/
+        TimeStamp();
+
+        /**
+           Constructs a TimeStamp representing the given offset since the
+           epoch ( 00:00:00 1970/1/1 UTC).
+        **/
+        TimeStamp(unsigned int seconds, unsigned int microSeconds = 0);
+
+        inline int getSeconds() const {
+            return _seconds;
+        };
+
+        inline int getMilliSeconds() const {
+            return _microSeconds / 1000;
+        };
+
+        inline int getMicroSeconds() const {
+            return _microSeconds;
+        };
+
+        static inline const TimeStamp& getStartTime() {
+            return _startStamp;
+        };
+
+        protected:
+        static TimeStamp _startStamp;
+
+        int _seconds;
+        int _microSeconds;
+    };
+}
+
+#endif // _LOG4CPP_TIMESTAMP_HH
+
