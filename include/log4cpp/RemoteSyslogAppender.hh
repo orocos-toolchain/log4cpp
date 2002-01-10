0646d3f6d1579ebdfcb48d738b348c61af6b7227
Bastiaan Bakker <bastiaan.bakker@lifeline.nl>
2002-01-10 16:45:10 +0000

    added SyslogFacility type.

--- include/log4cpp/RemoteSyslogAppender.hh
+++ include/log4cpp/RemoteSyslogAppender.hh
@@ -29,8 +29,32 @@ typedef enum {
     LOG_NOTICE  = 5,       ///< normal but significant condition
     LOG_INFO    = 6,       ///< informational
     LOG_DEBUG   = 7,       ///< debug-level messages
-    LOG_USER    = (1<<3)     ///< random user-level messages
 } SyslogLevel;
+
+typedef enum {
+    LOG_KERN     = (0<<3),  ///< kernel messages
+    LOG_USER     = (1<<3),  ///< random user-level messages
+    LOG_MAIL     = (2<<3),  ///< mail system
+    LOG_DAEMON   = (3<<3),  ///< system daemons
+    LOG_AUTH     = (4<<3),  ///< security/authorization messages
+    LOG_SYSLOG   = (5<<3),  ///< messages generated internally by syslogd
+    LOG_LPR      = (6<<3),  ///< line printer subsystem
+    LOG_NEWS     = (7<<3),  ///< network news subsystem
+    LOG_UUCP     = (8<<3),  ///< UUCP subsystem
+    LOG_CRON     = (9<<3),  ///< clock daemon
+    LOG_AUTHPRIV = (10<<3), ///< security/authorization messages (private)
+    LOG_FTP      = (11<<3), ///< ftp daemon
+    
+    /* other codes through 15 reserved for system use */ 
+    LOG_LOCAL0   = (16<<3), ///< reserved for local use
+    LOG_LOCAL1   = (17<<3), ///< reserved for local use
+    LOG_LOCAL2   = (18<<3), ///< reserved for local use
+    LOG_LOCAL3   = (19<<3), ///< reserved for local use
+    LOG_LOCAL4   = (20<<3), ///< reserved for local use
+    LOG_LOCAL5   = (21<<3), ///< reserved for local use
+    LOG_LOCAL6   = (22<<3), ///< reserved for local use
+    LOG_LOCAL7   = (23<<3), ///< reserved for local use
+} SyslogFacility;
 #endif
 
 namespace log4cpp {
