7cc444c3a63edb2924e7fbd5cfe1b6c63c14e8e3
Bastiaan Bakker <bastiaan.bakker@lifeline.nl>
2002-03-20 23:11:07 +0000

    Added documentation comments.

--- include/log4cpp/SimpleConfigurator.hh
+++ include/log4cpp/SimpleConfigurator.hh
@@ -20,9 +20,35 @@ namespace log4cpp {
         ConfigureFailure(const std::string& reason);
     };
 
+    /**
+     * This class implements a simple Configurator for log4cpp.
+     * It is a temporary hack with an undocumented configuration format.
+     * It should be superseded with a PropertyConfigurator and XMLConfigurator
+     * class in newer versions of log4cpp.
+     **/
     class LOG4CPP_EXPORT SimpleConfigurator {
     public:
+
+        /**
+         * Configure log4cpp with the configuration in the given file.
+         * NB. The configuration file format is undocumented and may change
+         * without notice.
+         * @since 0.2.6
+         * @param initFileName name of the configuration file
+         * @exception ConfigureFailure if the method encountered a read or 
+         * syntax error.
+         **/
         static void configure(const std::string& initFileName) throw (ConfigureFailure);
+
+        /**
+         * Configure log4cpp with the configuration in the given file.
+         * NB. The configuration file format is undocumented and may change
+         * without notice.
+         * @since 0.3.1
+         * @param initFile an input stream to the configuration file
+         * @exception ConfigureFailure if the method encountered a read or 
+         * syntax error.
+         **/
         static void configure(std::istream& initFile) throw (ConfigureFailure);    };
 }
 
