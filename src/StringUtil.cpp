87d063a11d3fc3a6a1cbb6ef1b4d56709b645b11
Bastiaan Bakker <bastiaan.bakker@lifeline.nl>
2002-07-05 17:10:19 +0000

    added split() method.

--- src/StringUtil.cpp
+++ src/StringUtil.cpp
@@ -25,4 +25,20 @@ namespace log4cpp {
         // return the remaining characters
         return std::string(s, b, e - b + 1);
     }
+
+    void StringUtil::split(std::vector<std::string>& v, const std::string& s,
+                           char delimiter, unsigned int maxSegments) {
+        v.empty();
+        std::string::size_type left = 0;
+        for(unsigned int i = 0; i < maxSegments - 1; i++) {
+            std::string::size_type right = s.find(delimiter, left);
+            if (right == std::string::npos) {
+                break;
+            }
+            v.push_back(s.substr(left, right - left));
+            left = right + 1;
+        }
+
+        v.push_back(s.substr(left));
+    }
 }
--- src/StringUtil.hh
+++ src/StringUtil.hh
@@ -11,16 +11,33 @@
 
 #include <log4cpp/Portability.hh>
 #include <string>
+#include <vector>
+#include <climits>
 
 namespace log4cpp {
 
-    /**
-       Returns a string identical to the given string but without leading or
-       trailing HTABs or spaces.
-    **/
     class StringUtil {
         public:
+        /**
+           Returns a string identical to the given string but without leading
+           or trailing HTABs or spaces.
+        **/
         static std::string trim(const std::string& s);
+
+        /**
+           splits a string into a vector of string segments based on the
+           given delimiter.
+           @param v The vector in which the segments will be stored. The vector
+           will be emptied before storing the segments
+           @param s The string to split into segments.
+           @param delimiter The delimiter character
+           @param maxSegments the maximum number of segments. Upon return
+           v.size() <= maxSegments.  The string is scanned from left to right
+           so v[maxSegments - 1] may contain a string containing the delimiter
+           character.
+        **/
+        static void split(std::vector<std::string>& v, const std::string& s,
+                          char delimiter, unsigned int maxSegments = INT_MAX);
     };
 }
 
