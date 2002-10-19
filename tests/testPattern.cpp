c166f8539b8ee02725d3b4aba75ad2f2a868c28e
Bastiaan Bakker <bastiaan.bakker@lifeline.nl>
2002-10-19 00:09:12 +0000

    added tests for format specifiers and more.

--- tests/testPattern.cpp
+++ tests/testPattern.cpp
@@ -1,57 +1,56 @@
 // testLog4CPP.cpp : Derived from testCategory.cpp
 //
-#include "log4cpp/Portability.hh"
-#ifdef LOG4CPP_HAVE_UNISTD_H
-#include <unistd.h>
-#endif
-
-#include "log4cpp/Category.hh"
-#include "log4cpp/Appender.hh"
-#include "log4cpp/OstreamAppender.hh"
-#include "log4cpp/FileAppender.hh"
-#include "log4cpp/Layout.hh"
-#include "log4cpp/BasicLayout.hh"
-#include "log4cpp/Priority.hh"
-#include "log4cpp/NDC.hh"
-#include "log4cpp/PatternLayout.hh"
+#include <log4cpp/Category.hh>
+#include <log4cpp/Appender.hh>
+#include <log4cpp/Priority.hh>
+#include <log4cpp/NDC.hh>
+#include <log4cpp/PatternLayout.hh>
+#include <log4cpp/BasicConfigurator.hh>
 
 void test(std::string pattern, log4cpp::PatternLayout* layout, log4cpp::Category& cat)
-{                                                                              
-	if (! layout->setConversionPattern(pattern)) {
-		std::cout << "bad pattern: \"" << pattern << '\"' << std::endl;
-    }                                                                          
-    cat.error("message");
+{
+    try {	
+        layout->setConversionPattern(pattern);
+        cat.error("message");
+    } catch(log4cpp::ConfigureFailure& f) {
+        std::cerr << "configure failure: " << f.what() << std::endl;
+    }	
 }                                                                              
 
 int main(int argc, char* argv[])
 {
-    log4cpp::Appender* appender = 
-        new log4cpp::OstreamAppender("default", &std::cout);
-
+    log4cpp::BasicConfigurator::configure();
     log4cpp::PatternLayout* layout = new log4cpp::PatternLayout();
-    appender->setLayout(layout);
+    log4cpp::Category::getRoot().getAppender()->setLayout(layout);
 
-    log4cpp::Category& root = log4cpp::Category::getRoot();
-    root.removeAllAppenders();
-    root.addAppender(appender);
-       root.setPriority(log4cpp::Priority::ERROR);
-    std::string pattern;
+    log4cpp::Category& cat = log4cpp::Category::getInstance("cat1");
     
-    test("%% %r %c:%d (%R / %r) [%p] %x %m %% (%u) %n", layout, root);
+    log4cpp::NDC::push("ndc1");
+
+    test("%% %r %c:%d (%R / %r) [%p] %x %m %% (%u) %n", layout, cat);
+
+    // test format specifiers
+    test(">%10m<%n", layout, cat);
+    test(">%-10m<%n", layout, cat);
+    test(">%3.5m<%n", layout, cat);
+    test(">%.5m<%n", layout, cat);
+
+    // category test
+    test(">%c{2}<%n", layout, log4cpp::Category::getInstance("c1.c2.c3.c4"));
 
     // test date format
-    test("%d{%d %b %Y %H:%M:%S.%l} %m %n", layout, root);
-    test("%d{%d %b %Y %H:%M:%S.%l", layout, root);
-    test("%d", layout, root);
+    test("%d{%d %b %Y %H:%M:%S.%l} %m %n", layout, cat);
+    test("%d{%d %b %Y %H:%M:%S.%l", layout, cat);
+    test("%d%n", layout, cat);
 
-    test("%m %d%n", layout, root);
+    test("%m %d%n", layout, cat);
     int i;
-    for (i = 0; i < 1000; i++) {
-        root.error("%d message", i);
+    for (i = 0; i < 10; i++) {
+        cat.error("%d message", i);
     }
-    test("%m %d{%d %b %Y %H:%M:%S.%l}%n", layout, root);
-    for (i = 0; i < 1000; i++) {
-        root.error("%d message", i);
+    test("%m %d{%H:%M:%S.%l %d %b %Y}%n", layout, cat);
+    for (i = 0; i < 10; i++) {
+        cat.error("%d message", i);
     }
 
     log4cpp::Category::shutdown();
