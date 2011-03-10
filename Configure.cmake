1d4e551147838076fb2584b63ab910ab0d4bec1c
Philippe Hamelin <philippe.hamelin@gmail.com>
2011-03-10 10:23:02 -0500

    Fix in source build.

--- Configure.cmake
+++ Configure.cmake
@@ -2,7 +2,7 @@
 # LOG4CPP_HAVE_SSTREAM
 ###########################################################
 
-FILE(WRITE "${CMAKE_CURRENT_BINARY_DIR}/configure/strstream.cpp.in"
+FILE(WRITE "${CMAKE_CURRENT_BINARY_DIR}/configure-tryouts/strstream.cpp.in"
 "#include <strstream>\n"
 "int main()\n"
 "{\n"
@@ -10,14 +10,14 @@ FILE(WRITE "${CMAKE_CURRENT_BINARY_DIR}/configure/strstream.cpp.in"
 "	os.str();\n"
 "	return 0;\n"
 "}")
-CONFIGURE_FILE(${CMAKE_CURRENT_BINARY_DIR}/configure/strstream.cpp.in ${CMAKE_CURRENT_BINARY_DIR}/configure/strstream.cpp)
-TRY_COMPILE(LOG4CPP_HAVE_SSTREAM ${CMAKE_BINARY_DIR} ${CMAKE_BINARY_DIR}/configure/strstream.cpp OUTPUT_VARIABLE OUTPUT)
+CONFIGURE_FILE(${CMAKE_CURRENT_BINARY_DIR}/configure-tryouts/strstream.cpp.in ${CMAKE_CURRENT_BINARY_DIR}/configure-tryouts/strstream.cpp)
+TRY_COMPILE(LOG4CPP_HAVE_SSTREAM ${CMAKE_BINARY_DIR} ${CMAKE_BINARY_DIR}/configure-tryouts/strstream.cpp OUTPUT_VARIABLE OUTPUT)
 
 ###########################################################
 # LOG4CPP_HAVE_GETTIMEOFDAY
 ###########################################################
 
-FILE(WRITE "${CMAKE_CURRENT_BINARY_DIR}/configure/gettimeofday.c.in"
+FILE(WRITE "${CMAKE_CURRENT_BINARY_DIR}/configure-tryouts/gettimeofday.c.in"
 "#include <stdlib.h> // for NULL on Linux\n"
 "#include <sys/time.h>\n"
 "int main()\n"
@@ -26,53 +26,53 @@ FILE(WRITE "${CMAKE_CURRENT_BINARY_DIR}/configure/gettimeofday.c.in"
 "	(void)gettimeofday(&tv, NULL);\n"
 "	return 0;\n"
 "}")
-CONFIGURE_FILE(${CMAKE_CURRENT_BINARY_DIR}/configure/gettimeofday.c.in ${CMAKE_CURRENT_BINARY_DIR}/configure/gettimeofday.c)
-TRY_COMPILE(LOG4CPP_HAVE_GETTIMEOFDAY ${CMAKE_BINARY_DIR} ${CMAKE_BINARY_DIR}/configure/gettimeofday.c OUTPUT_VARIABLE OUTPUT)
+CONFIGURE_FILE(${CMAKE_CURRENT_BINARY_DIR}/configure-tryouts/gettimeofday.c.in ${CMAKE_CURRENT_BINARY_DIR}/configure-tryouts/gettimeofday.c)
+TRY_COMPILE(LOG4CPP_HAVE_GETTIMEOFDAY ${CMAKE_BINARY_DIR} ${CMAKE_BINARY_DIR}/configure-tryouts/gettimeofday.c OUTPUT_VARIABLE OUTPUT)
 
 ###########################################################
 # LOG4CPP_HAVE_STDINT_H
 ###########################################################
 
-FILE(WRITE "${CMAKE_CURRENT_BINARY_DIR}/configure/stdint_h.c.in"
+FILE(WRITE "${CMAKE_CURRENT_BINARY_DIR}/configure-tryouts/stdint_h.c.in"
 "#include <stdint.h>\n"
 "int main()\n"
 "{\n"
 "	return 0;\n"
 "}")
-CONFIGURE_FILE(${CMAKE_CURRENT_BINARY_DIR}/configure/stdint_h.c.in ${CMAKE_CURRENT_BINARY_DIR}/configure/stdint_h.c)
-TRY_COMPILE(LOG4CPP_HAVE_STDINT ${CMAKE_BINARY_DIR} ${CMAKE_BINARY_DIR}/configure/stdint_h.c OUTPUT_VARIABLE OUTPUT)
+CONFIGURE_FILE(${CMAKE_CURRENT_BINARY_DIR}/configure-tryouts/stdint_h.c.in ${CMAKE_CURRENT_BINARY_DIR}/configure-tryouts/stdint_h.c)
+TRY_COMPILE(LOG4CPP_HAVE_STDINT ${CMAKE_BINARY_DIR} ${CMAKE_BINARY_DIR}/configure-tryouts/stdint_h.c OUTPUT_VARIABLE OUTPUT)
 
 ###########################################################
 # LOG4CPP_HAVE_UNISTD_H
 ###########################################################
 
-FILE(WRITE "${CMAKE_CURRENT_BINARY_DIR}/configure/unistd_h.c.in"
+FILE(WRITE "${CMAKE_CURRENT_BINARY_DIR}/configure-tryouts/unistd_h.c.in"
 "#include <unistd.h>\n"
 "int main()\n"
 "{\n"
 "	return 0;\n"
 "}")
-CONFIGURE_FILE(${CMAKE_CURRENT_BINARY_DIR}/configure/unistd_h.c.in ${CMAKE_CURRENT_BINARY_DIR}/configure/unistd_h.c)
-TRY_COMPILE(LOG4CPP_HAVE_UNISTD_H ${CMAKE_BINARY_DIR} ${CMAKE_BINARY_DIR}/configure/unistd_h.c OUTPUT_VARIABLE OUTPUT)
+CONFIGURE_FILE(${CMAKE_CURRENT_BINARY_DIR}/configure-tryouts/unistd_h.c.in ${CMAKE_CURRENT_BINARY_DIR}/configure-tryouts/unistd_h.c)
+TRY_COMPILE(LOG4CPP_HAVE_UNISTD_H ${CMAKE_BINARY_DIR} ${CMAKE_BINARY_DIR}/configure-tryouts/unistd_h.c OUTPUT_VARIABLE OUTPUT)
 
 ###########################################################
 # LOG4CPP_HAVE_IO_H
 ###########################################################
 
-FILE(WRITE "${CMAKE_CURRENT_BINARY_DIR}/configure/io_h.c.in"
+FILE(WRITE "${CMAKE_CURRENT_BINARY_DIR}/configure-tryouts/io_h.c.in"
 "#include <io.h>\n"
 "int main()\n"
 "{\n"
 "	return 0;\n"
 "}")
-CONFIGURE_FILE(${CMAKE_CURRENT_BINARY_DIR}/configure/io_h.c.in ${CMAKE_CURRENT_BINARY_DIR}/configure/io_h.c)
-TRY_COMPILE(LOG4CPP_HAVE_IO ${CMAKE_BINARY_DIR} ${CMAKE_BINARY_DIR}/configure/io_h.c OUTPUT_VARIABLE OUTPUT)
+CONFIGURE_FILE(${CMAKE_CURRENT_BINARY_DIR}/configure-tryouts/io_h.c.in ${CMAKE_CURRENT_BINARY_DIR}/configure-tryouts/io_h.c)
+TRY_COMPILE(LOG4CPP_HAVE_IO ${CMAKE_BINARY_DIR} ${CMAKE_BINARY_DIR}/configure-tryouts/io_h.c OUTPUT_VARIABLE OUTPUT)
 
 ###########################################################
 # LOG4CPP_HAVE_SNPRINTF
 ###########################################################
 
-FILE(WRITE "${CMAKE_CURRENT_BINARY_DIR}/configure/snprintf.c.in"
+FILE(WRITE "${CMAKE_CURRENT_BINARY_DIR}/configure-tryouts/snprintf.c.in"
 "#include <stdio.h>\n"
 "int main()\n"
 "{\n"
@@ -80,28 +80,28 @@ FILE(WRITE "${CMAKE_CURRENT_BINARY_DIR}/configure/snprintf.c.in"
 "   (void)snprintf(&x, sizeof(x), \"\");\n"
 "	return 0;\n"
 "}")
-CONFIGURE_FILE(${CMAKE_CURRENT_BINARY_DIR}/configure/snprintf.c.in ${CMAKE_CURRENT_BINARY_DIR}/configure/snprintf.c)
-TRY_COMPILE(LOG4CPP_HAVE_SNPRINTF ${CMAKE_BINARY_DIR} ${CMAKE_BINARY_DIR}/configure/snprintf.c OUTPUT_VARIABLE OUTPUT)
+CONFIGURE_FILE(${CMAKE_CURRENT_BINARY_DIR}/configure-tryouts/snprintf.c.in ${CMAKE_CURRENT_BINARY_DIR}/configure-tryouts/snprintf.c)
+TRY_COMPILE(LOG4CPP_HAVE_SNPRINTF ${CMAKE_BINARY_DIR} ${CMAKE_BINARY_DIR}/configure-tryouts/snprintf.c OUTPUT_VARIABLE OUTPUT)
 
 ###########################################################
 # LOG4CPP_HAVE_SYSLOG
 ###########################################################
 
-FILE(WRITE "${CMAKE_CURRENT_BINARY_DIR}/configure/syslog.c.in"
+FILE(WRITE "${CMAKE_CURRENT_BINARY_DIR}/configure-tryouts/syslog.c.in"
 "#include <syslog.h>\n"
 "int main()\n"
 "{\n"
 "   syslog(LOG_ERR, \"\");\n"
 "	return 0;\n"
 "}")
-CONFIGURE_FILE(${CMAKE_CURRENT_BINARY_DIR}/configure/syslog.c.in ${CMAKE_CURRENT_BINARY_DIR}/configure/syslog.c)
-TRY_COMPILE(LOG4CPP_HAVE_SYSLOG ${CMAKE_BINARY_DIR} ${CMAKE_BINARY_DIR}/configure/syslog.c OUTPUT_VARIABLE OUTPUT)
+CONFIGURE_FILE(${CMAKE_CURRENT_BINARY_DIR}/configure-tryouts/syslog.c.in ${CMAKE_CURRENT_BINARY_DIR}/configure-tryouts/syslog.c)
+TRY_COMPILE(LOG4CPP_HAVE_SYSLOG ${CMAKE_BINARY_DIR} ${CMAKE_BINARY_DIR}/configure-tryouts/syslog.c OUTPUT_VARIABLE OUTPUT)
 
 ###########################################################
 # LOG4CPP_HAVE_LOCALTIME_R
 ###########################################################
 
-FILE(WRITE "${CMAKE_CURRENT_BINARY_DIR}/configure/localtime_r.c.in"
+FILE(WRITE "${CMAKE_CURRENT_BINARY_DIR}/configure-tryouts/localtime_r.c.in"
 "#include <time.h>\n"
 "int main()\n"
 "{\n"
@@ -110,14 +110,14 @@ FILE(WRITE "${CMAKE_CURRENT_BINARY_DIR}/configure/localtime_r.c.in"
 "   (void)localtime_r(&t, &tm);\n"
 "	return 0;\n"
 "}")
-CONFIGURE_FILE(${CMAKE_CURRENT_BINARY_DIR}/configure/localtime_r.c.in ${CMAKE_CURRENT_BINARY_DIR}/configure/localtime_r.c)
-TRY_COMPILE(LOG4CPP_HAVE_LOCALTIME_R ${CMAKE_BINARY_DIR} ${CMAKE_BINARY_DIR}/configure/localtime_r.c OUTPUT_VARIABLE OUTPUT)
+CONFIGURE_FILE(${CMAKE_CURRENT_BINARY_DIR}/configure-tryouts/localtime_r.c.in ${CMAKE_CURRENT_BINARY_DIR}/configure-tryouts/localtime_r.c)
+TRY_COMPILE(LOG4CPP_HAVE_LOCALTIME_R ${CMAKE_BINARY_DIR} ${CMAKE_BINARY_DIR}/configure-tryouts/localtime_r.c OUTPUT_VARIABLE OUTPUT)
 
 ###########################################################
 # LOG4CPP_HAVE_FTIME
 ###########################################################
 
-FILE(WRITE "${CMAKE_CURRENT_BINARY_DIR}/configure/ftime.c.in"
+FILE(WRITE "${CMAKE_CURRENT_BINARY_DIR}/configure-tryouts/ftime.c.in"
 "#include <sys/timeb.h>\n"
 "int main()\n"
 "{\n"
@@ -125,23 +125,23 @@ FILE(WRITE "${CMAKE_CURRENT_BINARY_DIR}/configure/ftime.c.in"
 "   (void)ftime(&t);\n"
 "	return 0;\n"
 "}")
-CONFIGURE_FILE(${CMAKE_CURRENT_BINARY_DIR}/configure/ftime.c.in ${CMAKE_CURRENT_BINARY_DIR}/configure/ftime.c)
-TRY_COMPILE(LOG4CPP_HAVE_FTIME ${CMAKE_BINARY_DIR} ${CMAKE_BINARY_DIR}/configure/ftime.c OUTPUT_VARIABLE OUTPUT)
+CONFIGURE_FILE(${CMAKE_CURRENT_BINARY_DIR}/configure-tryouts/ftime.c.in ${CMAKE_CURRENT_BINARY_DIR}/configure-tryouts/ftime.c)
+TRY_COMPILE(LOG4CPP_HAVE_FTIME ${CMAKE_BINARY_DIR} ${CMAKE_BINARY_DIR}/configure-tryouts/ftime.c OUTPUT_VARIABLE OUTPUT)
 
 ###########################################################
 # LOG4CPP_HAVE_INT64_T
 ###########################################################
 
 IF (LOG4CPP_HAVE_STDINT)
-  FILE(WRITE "${CMAKE_CURRENT_BINARY_DIR}/configure/int64_t.c.in"
+  FILE(WRITE "${CMAKE_CURRENT_BINARY_DIR}/configure-tryouts/int64_t.c.in"
 	"#include <stdint.h>\n"
 	"int main()\n"
 	"{\n"
 	"   volatile int64_t t;\n"
 	"	return 0;\n"
 	"}")
-  CONFIGURE_FILE(${CMAKE_CURRENT_BINARY_DIR}/configure/int64_t.c.in ${CMAKE_CURRENT_BINARY_DIR}/configure/int64_t.c)
-  TRY_COMPILE(LOG4CPP_HAVE_INT64_T ${CMAKE_BINARY_DIR} ${CMAKE_BINARY_DIR}/configure/int64_t.c OUTPUT_VARIABLE OUTPUT)
+  CONFIGURE_FILE(${CMAKE_CURRENT_BINARY_DIR}/configure-tryouts/int64_t.c.in ${CMAKE_CURRENT_BINARY_DIR}/configure-tryouts/int64_t.c)
+  TRY_COMPILE(LOG4CPP_HAVE_INT64_T ${CMAKE_BINARY_DIR} ${CMAKE_BINARY_DIR}/configure-tryouts/int64_t.c OUTPUT_VARIABLE OUTPUT)
 ENDIF (LOG4CPP_HAVE_STDINT)
 
 
@@ -149,7 +149,7 @@ ENDIF (LOG4CPP_HAVE_STDINT)
 # LOG4CPP_HAVE_NAMESPACES
 ###########################################################
 
-FILE(WRITE "${CMAKE_CURRENT_BINARY_DIR}/configure/namespaces.cpp.in"
+FILE(WRITE "${CMAKE_CURRENT_BINARY_DIR}/configure-tryouts/namespaces.cpp.in"
 "#include <stdint.h>\n"
 "namespace test {\n"
 " int x=1;\n"
@@ -158,8 +158,8 @@ FILE(WRITE "${CMAKE_CURRENT_BINARY_DIR}/configure/namespaces.cpp.in"
 "{\n"
 "   return test::x;\n"
 "}")
-CONFIGURE_FILE(${CMAKE_CURRENT_BINARY_DIR}/configure/namespaces.cpp.in ${CMAKE_CURRENT_BINARY_DIR}/configure/namespaces.cpp)
-TRY_COMPILE(LOG4CPP_HAVE_NAMESPACES ${CMAKE_BINARY_DIR} ${CMAKE_BINARY_DIR}/configure/namespaces.cpp OUTPUT_VARIABLE OUTPUT)
+CONFIGURE_FILE(${CMAKE_CURRENT_BINARY_DIR}/configure-tryouts/namespaces.cpp.in ${CMAKE_CURRENT_BINARY_DIR}/configure-tryouts/namespaces.cpp)
+TRY_COMPILE(LOG4CPP_HAVE_NAMESPACES ${CMAKE_BINARY_DIR} ${CMAKE_BINARY_DIR}/configure-tryouts/namespaces.cpp OUTPUT_VARIABLE OUTPUT)
 
 # TODO
 #LOG4CPP_HAVE_BOOST
